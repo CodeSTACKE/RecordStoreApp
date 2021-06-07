861fd51b888fc0ef44e20bf73c0dc5d70eb24b20
Jyoti Das <48188772+CodeSTACKE@users.noreply.github.com>
2021-06-07 11:03:45 -0500

    fixed the warnings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import { Box, ChakraProvider, theme } from '@chakra-ui/react';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 import React from 'react';
 import {Footer, Header} from 'components';
 import{HomePage} from 'pages';
 import './index.css';
-import { scryRenderedComponentsWithType } from 'react-dom/test-utils';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import{ReactQueryDevtools} from 'react-query/devtools';
 
